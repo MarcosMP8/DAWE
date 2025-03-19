605843d43cbbcd29606751345a0dce7908196a81
Marcos Martín Paniagua <82602087+MarcosMP8@users.noreply.github.com>
2025-03-19 11:31:16 +0100

    Add files via upload

---
+++ Cabecera.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MenuNavegacion from "./MenuNavegacion.jsx";
+
+const Cabecera = () => {
+  return (
+    <header>
+      <h1>~ LA TIENDA DE FRIKIS ~</h1>
+      <MenuNavegacion />
+    </header>
+  );
+};
+
+export default Cabecera;
